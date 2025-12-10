e4b3751b2cd25b46ff23606162fdf0eb51ca0876
fishcg <353740902@qq.com>
2025-12-10 15:39:07 +0800

    add metrics alert

--- alert.js
+++ alert.js
@@ -1,5 +1,7 @@
+const { qywxAlert } = require("./config")
 const ExRateCheck = require("./lib/ExRateCheck")
 const HealthNotice = require("./lib/HealthNotice")
+const MetricsReport = require("./lib/MetricsReport")
 
 const cmdsStr = process.argv[2]
 if (cmdsStr === undefined) {
@@ -9,16 +11,19 @@ if (cmdsStr === undefined) {
 
 let cmds = cmdsStr.split(',')
 for (let cmd of cmds) {
-  switch (cmd) {
-    case 'ExRateCheck':
-      ExRateCheck.start()
-      break
-    case 'HealthNotice':
-      HealthNotice.start()
-      break
-    default:
-      console.log(`Cmd "${cmd}" is not supported`)
-      break
+  if (cmd === 'ExRateCheck') {
+    ExRateCheck.start()
+  } else if (cmd === 'HealthNotice') {
+    HealthNotice.start()
+  } else if (cmd.startsWith('Metrics')) {
+    // 以 metrics 开头的命令交给 MetricsReport 处理
+    let reportConfig = qywxAlert[cmd] || ''
+    if (!reportConfig) {
+      throw new Error(`Cmd "${cmd}" is not configured in qywxAlert`)
+    }
+    MetricsReport.start(reportConfig)
+  } else {
+    throw new Error(`Cmd "${cmd}" is not supported`)
   }
 }
 
